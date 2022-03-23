a8db3701f06203aa683a7b78755261e3b20b3d20
jamesanelay <jamesanelay@gmail.com>
2022-03-23 15:52:03 +0000

    Fix

--- app.py
+++ app.py
@@ -7,4 +7,6 @@ app = Flask(__name__)
 @app.route('/', methods=['POST'])
 def hello_world():
     serial = request.form.get('serial_number')
-    return 'Hello PineMedia!' + serial
+    ont_desc = request.form.get('ont_desc')
+
+    return 'Serial:' + serial + ' Description:' + ont_desc
